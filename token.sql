88c32f10f48015d523e18381ba688b5858bd8b11
Daniel Figueroa <danielmfigueroa@gmail.com>
2018-07-10 11:24:13 -0300

    Adição do arquivo do banco Mysql

---
+++ token.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2018 at 04:23 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `token`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unidade`
+--
+
+CREATE TABLE `unidade` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `logradouro` varchar(50) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `bairro` varchar(30) NOT NULL,
+  `cidade` varchar(30) NOT NULL,
+  `estado` varchar(30) NOT NULL,
+  `telefone` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unidade`
+--
+
+INSERT INTO `unidade` (`id`, `nome`, `logradouro`, `numero`, `bairro`, `cidade`, `estado`, `telefone`) VALUES
+(7, 'Unidade 01', 'EndereÃ§o teste 01', '01', 'Bairro 01', 'Cidade 01', 'Estado 01', '6299999999'),
+(8, 'Unidade 02', 'EndereÃ§o teste 02', '02', 'Bairro 02', 'Cidade 02', 'Estado 02', '62888888888');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `unidade`
+--
+ALTER TABLE `unidade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `unidade`
+--
+ALTER TABLE `unidade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
